9c66a3d09f7d4b05fb85b2c5ff66d654d29d78a9
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-28 22:03:06 +0200

    userdelete done

---
+++ UserProvider/Functions/DeleteUser.cs
@@ -0,0 +1,45 @@
+using Data.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace UserProvider.Functions;
+
+public class DeleteUser(ILogger<DeleteUser> logger, DataContext context)
+{
+    private readonly ILogger<DeleteUser> _logger = logger;
+    private readonly DataContext _context = context;
+
+    [Function("DeleteUser")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+       try
+        {
+            var userId = await req.ReadFromJsonAsync<UserIdModel>();
+            var user = await _context.Users.FindAsync(userId.Id);
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return new OkResult();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error deleting user");
+            return new BadRequestResult();
+        }
+    }
+
+    
+}
+public class UserIdModel
+{
+    public string Id { get; set; }
+}
