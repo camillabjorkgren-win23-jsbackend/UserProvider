b80ee6294dc90923d6e03845dd63ee90cc0c72ba
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2025-03-26 21:20:48 +0100

    changes

--- Data/Contexts/DataContextFactory.cs
+++ Data/Contexts/DataContextFactory.cs
@@ -13,7 +13,7 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:camilla-bjorkgren-silicon-server.database.windows.net,1433;Initial Catalog=camillabjorkgren-silicon-sql;Persist Security Info=False;User ID=camillabjorkgrensqlserver;Password=LykkeFrank25!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("ConnectionString"));
 
         return new DataContext(optionsBuilder.Options);
     }
